8e4f97699503e54241983f339633c0d7aaca7c45
Xelpi <ankith3690@gmail.com>
2025-11-01 17:40:11 +0530

    Fixed the images

---
+++ static/js/sourcemaps/ae1a623eec1ec5d4.js
@@ -0,0 +1,8 @@
+(globalThis["webpackChunk_canva_web"] = globalThis["webpackChunk_canva_web"] || []).push([[37147],{
+
+/***/ 318747:
+function(_, __, __webpack_require__) {__webpack_require__.n_x = __webpack_require__.n;const __web_req__ = __webpack_require__;__web_req__(905716);globalThis._5f74ec40302898c5a55451c9fbd04240 = globalThis._5f74ec40302898c5a55451c9fbd04240 || {};(function(__c) {__c.VOb={config:{language:"en-GB",Lf:{yMMMd:"d MMM yyyy",yMd:"dd/MM/yyyy",yMMM:"MMM yyyy"},Zf:"Jan Feb Mar Apr May Jun Jul Aug Sept Oct Nov Dec".split(" "),$f:"January February March April May June July August September October November December".split(" "),Gg:[{pattern:"dd *[/-] *mm *[/-] *yy",na:"yMd"},{pattern:"dd *[/-] *mm *[/-] *yyyy",na:"yMd"},{pattern:"dd *[/-] *mm",na:"yMd"}]}};
+}).call(globalThis, globalThis._5f74ec40302898c5a55451c9fbd04240);}
+
+}])
+//# sourceMappingURL=sourcemaps/ae1a623eec1ec5d4.js.map
